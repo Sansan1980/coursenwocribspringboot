0d671ec673f373746ae3816d17631e7b7bccce03
Sansan1980 <152459976+Sansan1980@users.noreply.github.com>
2024-03-01 07:00:21 +0300

    counter

--- src/main/java/com/example/demo/controller/CounterController.java
+++ src/main/java/com/example/demo/controller/CounterController.java
@@ -1,12 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CounterController {
+    private int count;
 
     @GetMapping
     public String hello() {
         return "Hello, world!";
+    }
+
+    @GetMapping("/counter")
+    public String count() {
+        this.count++;
+        return "Количество запросов: " + this.count;
     }
 }
